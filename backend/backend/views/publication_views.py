e2cbd1f03e68d82543ceb741310e358b9b267880
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2025-06-24 11:00:33 -0400

    Fixed formatting

--- backend/backend/views/publication_views.py
+++ backend/backend/views/publication_views.py
@@ -1,6 +1,7 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from ..models.publication import Publication
 from ..serializers.publication_serializer import PublicationSerializer
 
