6b05ea1509cf6da2a335716483e7e0df365669e2
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2025-07-20 10:45:14 -0400

    [Fix]: Fix formatting

--- backend/backend/admin/__init__.py
+++ backend/backend/admin/__init__.py
@@ -2,4 +2,4 @@ from .award_admin import AwardAdmin, AwardRecipientAdmin
 from .event_admin import EventAdmin, EventParticipantAdmin
 from .member_admin import MemberAdmin
 from .publication_admin import PublicationAdmin
-from .research_project_admin import ResearchProjectAdmin, ProjectParticipantAdmin
+from .research_project_admin import ProjectParticipantAdmin, ResearchProjectAdmin
