1a4f70d8cdabcdbee3dee24cfb9ae3d9a4afd98c
Hugo <hugo.rheaume@hotmail.com>
2025-07-21 11:21:26 -0400

    Added swagger & login/register custom swagger docs

--- backend/backend/test/test_auth.py
+++ backend/backend/test/test_auth.py
@@ -69,7 +69,7 @@ def test_login_success_email(mock_user_filter, mock_authenticate, login_url):
     mock_user = MagicMock(spec=User)
     mock_user.username = "dev"
     mock_user.email = "dev@example.com"
-    mock_user.id = "some-uuid"
+    mock_user.id = 1
     mock_user.first_name = "dev"
     mock_user.last_name = "dev"
     mock_user.role = "developer"
@@ -94,7 +94,7 @@ def test_login_success_username(mock_user_filter, mock_authenticate, login_url):
     mock_user = MagicMock(spec=User)
     mock_user.username = "dev"
     mock_user.email = "dev@example.com"
-    mock_user.id = "some-uuid"
+    mock_user.id = 1
     mock_user.first_name = "dev"
     mock_user.last_name = "dev"
     mock_user.role = "developer"
--- backend/backend/views/auth_views.py
+++ backend/backend/views/auth_views.py
@@ -1,12 +1,22 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from ..serializers.auth_serializer import LoginSerializer, RegisterSerializer
+from ..serializers.auth_serializer import (
+    LoginResponseSerializer,
+    LoginSerializer,
+    RegisterSerializer,
+)
 
 
 class RegisterView(APIView):
+    @swagger_auto_schema(
+        operation_description="Register as a new user and member",
+        request_body=RegisterSerializer,
+        responses={201: RegisterSerializer},
+    )
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
@@ -19,22 +29,22 @@ class RegisterView(APIView):
 
 
 class LoginView(APIView):
+    @swagger_auto_schema(
+        operation_description="Login with username or email and password",
+        request_body=LoginSerializer,
+        responses={200: LoginResponseSerializer},
+    )
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.validated_data["user"]
             refresh = RefreshToken.for_user(user)
-
-            return Response(
-                {
-                    "refresh_token": str(refresh),
-                    "access_token": str(refresh.access_token),
-                    "user": {
-                        "id": str(user.id),
-                        "username": user.username,
-                        "email": user.email,
-                    },
-                }
-            )
+            response_data = {
+                "refresh_token": str(refresh),
+                "access_token": str(refresh.access_token),
+                "user": user,
+            }
+            response_serializer = LoginResponseSerializer(response_data)
+            return Response(response_serializer.data)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
