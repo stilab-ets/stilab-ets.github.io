50f82e3075229f92c59bfb781ce02317ecd5b795
Alexander Barcenes <alexanderbarcenes@gmail.com>
2025-07-26 15:20:00 -0400

    Adding missing endpoints for awards and courses as well as unit tests

--- backend/backend/test/test_courses.py
+++ backend/backend/test/test_courses.py
@@ -24,7 +24,7 @@ def test_courses_get_success(mock_get_object, mock_serializer, courses_url):
     mock_get_object.return_value = mock_course
 
     mock_serializer_instance = MagicMock()
-    mock_serializer_instance.data = [{"title": "Dev", "url": "Dev"}]
+    mock_serializer_instance.data = [{"title": "Dev", "url": "Test"}]
     mock_serializer.return_value = mock_serializer_instance
 
     response = client.get(courses_url)
@@ -37,23 +37,92 @@ def test_courses_get_success(mock_get_object, mock_serializer, courses_url):
 
 
 @patch("backend.views.courses_view.CourseSerializer")
-@patch("backend.views.courses_view.get_list_or_404")
-def test_courses_get_multiple_success(mock_get_object, mock_serializer, courses_url):
-    mock_courses = [MagicMock(), MagicMock()]
-    mock_get_object.return_value = mock_courses
+@patch("backend.views.courses_view.Course")
+def test_courses_post_success(mock_course_model, mock_serializer, courses_url):
+    mock_queryset_1 = MagicMock()
+    mock_queryset_2 = MagicMock()
+    mock_combined_queryset = MagicMock()
 
+    # Each filter call returns a separate mock
+    mock_course_model.objects.filter.side_effect = [mock_queryset_1, mock_queryset_2]
+
+    # The OR operation between querysets returns a combined queryset mock
+    mock_queryset_1.__or__.return_value = mock_combined_queryset
+    mock_combined_queryset.exists.return_value = False
+
+    mock_course_instance = MagicMock()
     mock_serializer_instance = MagicMock()
-    mock_serializer_instance.data = [
-        {"title": "Dev1", "url": "Dev1"},
-        {"title": "Dev2", "url": "Dev2"},
-    ]
+
+    mock_serializer_instance.is_valid.return_value = True
+    mock_serializer_instance.save.return_value = mock_course_instance
+    mock_serializer_instance.data = {"title": "Dev", "url": "Test"}
     mock_serializer.return_value = mock_serializer_instance
 
-    response = client.get(courses_url)
+    mock_course_model.objects.create.return_value = mock_course_instance
+
+    data = {"title": "Dev", "url": "Test", "year": 2025, "organization": "test"}
+
+    client.force_authenticate(user=MagicMock())
+    response = client.post(courses_url, data, format="json")
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data["title"] == "Dev"
+
+    assert mock_course_model.objects.filter.call_count == 2
+    mock_combined_queryset.exists.assert_called_once()
+
+    mock_serializer.assert_called_once_with(data=data)
+    mock_serializer_instance.is_valid.assert_called_once()
+    mock_serializer_instance.save.assert_called_once()
+
+
+@patch("backend.views.courses_view.CourseSerializer")
+@patch("backend.views.courses_view.Course")
+def test_courses_put_success(mock_course_model, mock_serializer, courses_url):
+    mock_queryset_1 = MagicMock()
+    mock_queryset_2 = MagicMock()
+    mock_combined_queryset = MagicMock()
+
+    mock_course_model.objects.filter.side_effect = [mock_queryset_1, mock_queryset_2]
+    mock_queryset_1.__or__.return_value = mock_combined_queryset
+    mock_combined_queryset.exists.return_value = True
+
+    mock_course_instance = MagicMock()
+    mock_combined_queryset.first.return_value = mock_course_instance
+
+    mock_serializer_instance = MagicMock()
+    mock_serializer_instance.is_valid.return_value = True
+    mock_serializer_instance.save.return_value = mock_course_instance
+    mock_serializer_instance.data = {"title": "Dev", "url": "Test"}
+    mock_serializer.return_value = mock_serializer_instance
+
+    data = {"id": 1, "title": "Dev", "url": "Test"}
+
+    client.force_authenticate(user=MagicMock())
+    response = client.put(courses_url, data, format="json")
 
     assert response.status_code == status.HTTP_200_OK
-    assert len(response.data) == 2
-    assert response.data[0]["title"] == "Dev1"
-    assert response.data[1]["title"] == "Dev2"
-    mock_get_object.assert_called_once()
-    mock_serializer.assert_called_once_with(mock_courses, many=True)
+    assert response.data["title"] == "Dev"
+
+    assert mock_course_model.objects.filter.call_count == 2
+    mock_combined_queryset.exists.assert_called_once()
+    mock_combined_queryset.first.assert_called_once()
+
+    mock_serializer.assert_called_once_with(instance=mock_course_instance, data=data)
+    mock_serializer_instance.is_valid.assert_called_once()
+    mock_serializer_instance.save.assert_called_once()
+
+
+@patch("backend.views.courses_view.get_object_or_404")
+def test_courses_delete_success(mock_get_object_or_404, courses_url):
+    mock_course_instance = MagicMock()
+    mock_get_object_or_404.return_value = mock_course_instance
+
+    data = {"id": "1", "title": "Dev", "url": "Test"}
+    client.force_authenticate(user=MagicMock())
+    response = client.delete(courses_url, data, format="json")
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    mock_get_object_or_404.assert_called_once()
+    mock_course_instance.delete.assert_called_once()
--- backend/backend/views/awards_view.py
+++ backend/backend/views/awards_view.py
@@ -1,5 +1,7 @@
+from django.shortcuts import get_list_or_404, get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,6 +10,11 @@ from ..serializers.award_serializer import AwardSerializer
 
 
 class AwardsView(APIView):
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
     @swagger_auto_schema(
         operation_id="Get Awards",
         operation_description="Retrieves a list of all awards",
@@ -15,6 +22,61 @@ class AwardsView(APIView):
         tags=["Award"],
     )
     def get(self, request):
-        awards = Award.objects.all()
+        awards = get_list_or_404(Award)
         serializer = AwardSerializer(awards, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_id="Create Awards",
+        operation_description="Creates a new award",
+        responses={201: AwardSerializer},
+        tags=["Awards"],
+    )
+    def post(self, request):
+        existing = Award.objects.filter(id=request.data.get("id")) | Award.objects.filter(citekey=request.data.get("citekey"))
+
+        if existing.exists():
+            return Response(
+                {"error": "An award with this ID or citekey already exists."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer = AwardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        operation_id="Update Awards",
+        operation_description="Updates an award",
+        responses={200: AwardSerializer},
+        tags=["Awards"],
+    )
+    def put(self, request):
+        existing = Award.objects.filter(id=request.data.get("id")) | Award.objects.filter(citekey=request.data.get("citekey"))
+
+        if not existing.exists():
+            return Response(
+                {"error": "The award you are trying to update does not exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        award = existing.first()
+        serializer = AwardSerializer(instance=award, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        operation_id="Delete awards",
+        operation_description="Deletes an award",
+        responses={200: AwardSerializer},
+        tags=["Awards"],
+    )
+    def delete(self, request):
+        award = get_object_or_404(Award, id=request.data.get("id"))
+        award.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
